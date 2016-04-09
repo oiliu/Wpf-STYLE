730c8e1ac0984ba7e9732ff7f36bdcf1a6403334
oiliu <755422922@qq.com>
2016-04-09 10:39:54 +0800

    加减号数字改变

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Wpf-STYLE
 
 ![example](https://raw.githubusercontent.com/oiliu/Wpf-STYLE/master/img/jia'shi's.gif)
+
+![加减号控制数字大小](https://raw.githubusercontent.com/oiliu/Wpf-STYLE/master/img/2.gif)
---
+++ 数字加减文本框/WinButtonTextbox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHeatMap
+{
+    /// <summary>
+    /// WinButtonTextbox.xaml 的交互逻辑
+    /// </summary>
+    public partial class WinButtonTextbox : Window
+    {
+        public WinButtonTextbox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox txtBox1temp = (TextBox)txtBox1.Template.FindName("txtBox2", txtBox1);
+            txtBox1temp.Text = (Convert.ToInt32(txtBox1temp.Text) + 1).ToString();
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox txtBox1temp = (TextBox)txtBox1.Template.FindName("txtBox2", txtBox1);
+            if (Convert.ToInt32(txtBox1temp.Text) > 1)
+                txtBox1temp.Text = (Convert.ToInt32(txtBox1temp.Text) - 1).ToString();
+        }
+    }
+}
