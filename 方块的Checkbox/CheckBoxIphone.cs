21cc32cc1a6223f68fbbedfd66a59d957305c8c9
oiliu <755422922@qq.com>
2016-05-04 15:40:10 +0800

    旋转

---
+++ 方块的Checkbox/CheckBoxIphone.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace RigourTech.Football
+{
+	class CheckBoxIphone : CheckBox
+	{
+		Storyboard sbMoveToRight;
+		Storyboard sbMoveToLeft;
+		Storyboard sbRectangleShow;
+		Storyboard sbRectangleHide;
+		Rectangle ellipse;
+		Rectangle rectangle;
+        
+        public CheckBoxIphone()
+        {
+            Style styleCheckBox = this.FindResource("CheckBoxStyleIP") as Style;
+            if (styleCheckBox != null)
+            {
+                this.Style = styleCheckBox;
+            }
+
+            sbMoveToRight = this.FindResource("SB_EllipseMoveToRight") as Storyboard;
+            sbMoveToLeft = this.FindResource("SB_EllipseMoveToLeft") as Storyboard;
+            sbRectangleShow = this.FindResource("SB_RectangleShow") as Storyboard;
+            sbRectangleHide = this.FindResource("SB_RectangleHide") as Storyboard;
+        }
+
+        public CheckBoxIphone(UserControl mainWnd)
+		{
+			Style styleCheckBox = mainWnd.Resources["CheckBoxStyleIP"] as Style;
+			if (styleCheckBox != null)
+			{
+				this.Style = styleCheckBox;
+			}
+
+			sbMoveToRight = this.Template.Resources["SB_EllipseMoveToRight"] as Storyboard;
+			sbMoveToLeft = this.Template.Resources["SB_EllipseMoveToLeft"] as Storyboard;
+			sbRectangleShow = this.Template.Resources["SB_RectangleShow"] as Storyboard;
+			sbRectangleHide = this.Template.Resources["SB_RectangleHide"] as Storyboard;
+		}
+
+		protected override void OnChecked(RoutedEventArgs e)
+		{
+			ellipse = this.Template.FindName("ellipse", this) as Rectangle;
+			rectangle = this.Template.FindName("rectangleWhite", this) as Rectangle;
+			base.OnChecked(e);
+			if (sbMoveToRight != null)
+			{
+				sbMoveToRight.Begin(ellipse);
+			}
+			if (sbRectangleHide != null)
+			{
+				sbRectangleHide.Begin(rectangle,true);
+			}
+		}
+
+		protected override void OnUnchecked(RoutedEventArgs e)
+		{
+			base.OnUnchecked(e);
+			if (sbMoveToLeft != null)
+			{
+				sbMoveToLeft.Begin(ellipse);
+				if (sbRectangleShow != null)
+				{
+					sbRectangleShow.Begin(rectangle, true);
+				}
+			}
+		}
+	}
+}
---
+++ 旋转/WinTransform.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHeatMap
+{
+    /// <summary>
+    /// WinTransform.xaml 的交互逻辑
+    /// </summary>
+    public partial class WinTransform : Window
+    {
+        public WinTransform()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            RoteTransformGrid(90, 0);
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            RoteTransformGrid(0, 90);
+        }
+
+        private void RoteTransformGrid(double from,double to)
+        {
+            RotateTransform rtf = new RotateTransform();
+            btn_transForm.RenderTransform = rtf;
+            DoubleAnimation dbAscending = new DoubleAnimation(from, to, new Duration(TimeSpan.FromSeconds(1)));
+            rtf.BeginAnimation(RotateTransform.AngleProperty, dbAscending);
+        }
+    }
+}
